92a2e81285158f6436f8c4810f6f86082665fdc7
mapleFU <1506118561@qq.com>
2018-11-30 14:16:01 +0800

    [ADD] add basic logic for boot container

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
+import os
+
 import docker
 
+env_dict = {
+    "CQHTTP_SERVE_DATA_FILES": "yes",
+    "CQHTTP_SERVE_DATA_FILES":"yes",
+    # 尚未实现
+    "CQHTTP_USE_WS": "no",
+    "CQHTTP_USE_HTTP": "yes",
+    "CQHTTP_POST_MESSAGE_FORMAT": "array"
+}
+
+CQHTTP_POST_URL: str = "CQHTTP_POST_URL"
+COOLQ_ACCOUNT: str = "COOLQ_ACCOUNT"
+
+
 if __name__ == '__main__':
-    client = docker.from_env()
-    resp = client.containers.run("alpine:3.8", "echo hello world")
-    print(resp)
-    print(client.images.list())
+    try:
+        # 获得环境变量的 上报地址
+        post_url: str = os.environ.get("CQHTTP_POST_URL")
+        # 跑到的 account
+        coolq_account: str = os.environ.get("COOLQ_ACCOUNT")
+        # 事件接收的端口
+        event_recv_port: int = int(os.environ.get("EVENT_RECV_PORT", "5700"))
+        expose_login_port: int = int(os.environ.get("EXPOSE_LOGIN_PORT", "9005"))
+        pwd = os.getcwd()
+
+        # build env dict
+        env_dict[CQHTTP_POST_URL] = post_url
+        env_dict[COOLQ_ACCOUNT] = coolq_account
+
+        client = docker.from_env()
+        # 这是一个阻塞调用
+        resp = client.containers.run("richardchien/cqhttp:latest",
+                                     ports={
+                                         "9000/tcp": expose_login_port,
+                                         "5700/tcp": event_recv_port,
+                                     }, environment=env_dict,
+                                     volumes={
+                                        os.path.join(pwd, "coolq"): {
+                                            "bind": "/home/user/coolq",
+                                            "mode": "rw",
+                                        },
+                                     })
+        print(resp)
+    except Exception as e:
+        raise e
+
