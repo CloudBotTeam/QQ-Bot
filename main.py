97a57a82e9f7d04da6d7bd2a8f46d5dd1f88e414
mapleFU <1506118561@qq.com>
2018-12-02 04:23:46 +0800

    [ADD] add dynamically write data and boot with heartbeat

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import configparser
 
 import docker
 
 env_dict = {
     "CQHTTP_SERVE_DATA_FILES": "yes",
-    "CQHTTP_SERVE_DATA_FILES":"yes",
+    "CQHTTP_SERVE_DATA_FILES": "yes",
     # 尚未实现
     "CQHTTP_USE_WS": "no",
     "CQHTTP_USE_HTTP": "yes",
@@ -18,17 +19,41 @@ COOLQ_ACCOUNT: str = "COOLQ_ACCOUNT"
 if __name__ == '__main__':
     try:
         # 获得环境变量的 上报地址
-        post_url: str = os.environ.get("CQHTTP_POST_URL")
+        post_url: str = os.environ.get(CQHTTP_POST_URL, "http://10.0.0.229:8083/event")
         # 跑到的 account
-        coolq_account: str = os.environ.get("COOLQ_ACCOUNT")
+        coolq_account: str = os.environ.get(COOLQ_ACCOUNT, "3187545268")
         # 事件接收的端口
         event_recv_port: int = int(os.environ.get("EVENT_RECV_PORT", "5700"))
         expose_login_port: int = int(os.environ.get("EXPOSE_LOGIN_PORT", "9005"))
+        # 心跳 相关的事件
+        enable_heart_beat = bool(os.environ.get("ENABLE_HEART_BEAT", "true"))
+        heartbeat_interval = int(os.environ.get("HEARTBEAT_INTERVAL", "5000"))
+
         pwd = os.getcwd()
 
         # build env dict
         env_dict[CQHTTP_POST_URL] = post_url
         env_dict[COOLQ_ACCOUNT] = coolq_account
+
+        if enable_heart_beat:
+            env_dict["CQHTTP_ENABLE_HEART_BEAT"] = enable_heart_beat
+            env_dict["CQHTTP_HEARTBEAT_INTERVAL"] = heartbeat_interval
+
+        if coolq_account:
+            # write config file
+            config = configparser.ConfigParser()
+            config[coolq_account] = {}
+            config_ini = config[coolq_account]
+            config_ini["port"] = '5700'
+            config_ini["use_http"] = 'yes'
+            config_ini["post_url"] = post_url
+            config_ini["post_message_format"] = "array"
+            config_ini["enable_heartbeat"] = "true"
+            config_ini["heartbeat_interval"] = str(heartbeat_interval)
+            with open("coolq/app/io.github.richardchien.coolqhttpapi/config/{}.ini".format(coolq_account),
+                      encoding='utf-8',
+                      mode='w') as f:
+                config.write(f)
 
         client = docker.from_env()
         # 这是一个阻塞调用
@@ -42,8 +67,15 @@ if __name__ == '__main__':
                                             "bind": "/home/user/coolq",
                                             "mode": "rw",
                                         },
-                                     })
-        print(resp)
+                                     }, detach=True, network='robot-network')
+        try:
+            for line in resp.logs(stream=True):
+                line = line.strip()
+                if line:
+                    print(line.decode('utf-8'))
+        except KeyboardInterrupt as e:
+            resp.stop()
+            raise e
     except Exception as e:
         raise e
 
